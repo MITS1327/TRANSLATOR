6d8b9e6b2feca7b9d3e9a3e027cf8a44759c2c38
maxbronnikov10 <bronnikov@mcn.ru>
2022-08-26 08:05:53 +0000

    Fix determinate project

--- api/src/common/decorators/project.decorator.ts
+++ api/src/common/decorators/project.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const Project = createParamDecorator((data: unknown, ctx: ExecutionContext): string => {
   const request = ctx.switchToHttp().getRequest();
-  const project: string = request.headers.referer?.match(/https{0,1}:\/\/([a-z]*)-[a-z]*.[a-z]*.[a-z]{2,5}/);
+  const project: string = request.headers.referer?.match(/https{0,1}:\/\/([a-z]*)(-[a-z]* | *).[a-z]*.[a-z]{2,5}/);
 
   return project?.[1];
 });
