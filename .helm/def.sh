ba12fd1c73ad8d5dd0275c1d1ff179d9d4eef119
maxbronnikov10 <bronnikov@mcn.ru>
2022-09-08 13:51:08 +0300

    Add configmap and fix graylog

--- .helm/def.sh
+++ .helm/def.sh
@@ -2,7 +2,7 @@
 #   prod - конфигурация "Продакшн", приложение смотрит на боевой сервер.
 #   dev* - конфигурация для разработки, запускаются база и пгадмин 
 
-export TAG=1.007
+export TAG=1.008
 export APPNAME=translator
 export NAMESPACE=translator
 
