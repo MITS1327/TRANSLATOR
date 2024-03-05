5742a2a5630a2c628cb4c94d653f4c4a16b9e8c9
Maxim Bronnikov <maxbronnikov2004@gmail.com>
2024-03-05 15:14:23 +0000

    feat: Pootle file mime type not on linux

--- app/api/src/project/constants.ts
+++ app/api/src/project/constants.ts
@@ -1,3 +1,3 @@
-export const MAX_FILE_SIZE = 1024 * 1024 * 5; // 5MB
+export const MAX_FILE_SIZE = 1024 * 1024; // 1MB
 export const MAX_FILES_COUNT = 10;
-export const FILE_TYPE = 'text/x-gettext-translation';
+export const FILE_TYPE = 'text/x-gettext-translation|application/octet-stream';
